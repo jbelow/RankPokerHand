29070d918e4cf6ff8714cc68e27db25f0acf3641
Joshua Below <33503583+jbelow@users.noreply.github.com>
2020-09-22 17:58:26 -0500

    forgot to commit the junit testing

--- src/CardsAndHandsTest.java
+++ src/CardsAndHandsTest.java
@@ -1,8 +1,30 @@
 import org.junit.Test;
 
+import javax.swing.text.Utilities;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CardsAndHandsTest {
+
+    private Utilities utility;
+
+    @org.junit.Before
+    public  void before() {
+        utility = new Utilities();
+    }
+
+    @Test
+    public void setSuit() {
+        assertEquals("this is wrong", utility.setSuit());
+    }
+
+    @Test
+    public void setRank() {
+    }
+
+    @Test
+    public void checkHand() {
+    }
 
 //    @Test
 //    public
